adb49b68b6b24be29f1031610d801daa625da6e2
yufuworks <yufuworks@outlook.jp>
2023-03-22 14:23:28 +0000

    rust study

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let vec1: Vec<i32> = Vec::new();
+    let mut vec2: Vec<i32> = vec![1, 2, 3, 4, 5];
+    vec2.push(6);
+    println!("1st : {}", vec2[0]);
+    let second: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("2nd : {}", second),
+        None => println!("No 2nd value"),
+    }
+    for i in &mut vec2 {
+        *i *= 2;
+    }
+    for i in &vec2 {
+        println!("{}", i);
+    }
+    println!("Vec Length {}", vec2.len());
+    println!("Pop : {:?}", vec2.pop());
 }
